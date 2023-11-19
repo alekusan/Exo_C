107655e9654e70a1c268eac2db2741c5361cc2cc
alekusan <diazalexandre67pro@gmail.com>
2023-11-19 13:34:35 +0100

    Ajout de l'exercice 1 sur les pointeurs

---
+++ pointeurs/exercice_1/main.c
@@ -0,0 +1,16 @@
+//Déclarez un pointeur pour un type de base (comme int ou char) 
+//et initialisez-le avec l'adresse d'une variable de ce type
+
+int main() {
+    
+    //initialisation de la variable var
+    int var = 10;
+    
+    //initialisation du pointeur "ptr" sur "var" 
+    int* ptr = &var;
+
+    //affichage de la valeur et adresse de ptr 
+    printf("-adresse du pointeur: %p\n-valeur du pointeur %d", ptr, *ptr);
+
+    return 0 ;
+}
